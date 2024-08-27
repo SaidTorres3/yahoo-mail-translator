11acbcda410e532957722a0e252551ba17ba42f9
Said Torres <sesaid17@hotmail.com>
2024-08-27 09:58:21 -0600

    Support data query params on the url and increased fetching time to 10 seconds

--- updateInbox.js
+++ updateInbox.js
@@ -1,14 +1,18 @@
 (function () {
   function updateInbox() {
-    const targetDiv = document.querySelector('.D_F.p_R.k_w.r_P.H_6NIX.c1AVi7a_6UbO.W_6D6F.X_6DEy.N_fq7.ab_C.I4_2tvNsT.n_Z1YKD9t.I_1kC8gG.C_ZkbNhI.is_26ISAR.cZdTOHS_ZXgLQ3[data-test-folder-container="Inbox"]');
+    const targetDiv = document.querySelector('span[data-test-id="folder-label"]');
     if (targetDiv) {
       targetDiv.click();
     }
   }
 
   setInterval(() => {
-    if (window.location.href === "https://mail.yahoo.com/d/folders/1") {
+    const currentUrl = window.location.href;
+    const baseUrl = "https://mail.yahoo.com/d/folders/1";
+    const urlWithoutParams = currentUrl.split('?')[0];
+
+    if (urlWithoutParams === baseUrl) {
       updateInbox();
     }
-  }, 7000);
+  }, 10000);
 })();
